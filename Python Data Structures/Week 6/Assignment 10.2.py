de04f8b1e01159d404f5c07b7a7fc4676350f643
Rashik Wasik <rashikrajin@gmail.com>
2021-04-28 00:00:24 +0600

    Update Assignment 10.2.py

--- Python Data Structures/Week 6/Assignment 10.2.py
+++ Python Data Structures/Week 6/Assignment 10.2.py
@@ -26,6 +26,42 @@ for a in li:
 for k,v in di.items():
     print(k,v)
 
+
     
+    
+    
+    
+    
+
+    
+    
+# To sort the dictionary instead of the list
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+
+li = list()
+
+with open(name,"r") as textfile:
+    for i in textfile:
+        if i.startswith("From "):
+            asd = i.split()
+            jkl = asd[5].split(":")
+            li.append(jkl[0])
+            
+di = dict()
+
+for a in li:
+    di[a] = di.get(a,0) + 1
+    
+di = sorted([(k,v) for k,v in di.items()])  # It returns a list 
+    
+for k,v in di:
+    print(k,v)
+
+ 
+        
+
+
         
 
